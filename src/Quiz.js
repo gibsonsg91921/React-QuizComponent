fc4b4630f7531eda13986939406e0247e562c0cb
Adam <adam.wisbrock@perficient.com>
2018-08-02 13:58:58 -0500

    fixing module 6

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,7 +10,7 @@ class Quiz extends Component {
     this.state = {quiz_position: 1};
   }
   showNextQuestion() {
-    this.setState((prevState) => {quiz_position: prevState+1});
+    this.setState({quiz_position: this.state.quiz_position+1});
   }
   render() {
     const isQuizEnd = (this.state.quiz_position - 1) == quizData.quiz_questions.length;
